a36b6ca4753d82fcab74d98785e9c0abe4e8a053
Suraj Ravishankar <120905101+SurajCodes99@users.noreply.github.com>
2024-03-01 11:31:48 +0530

    Testing if git works or not.

--- RatingService/RatingService/src/main/java/com/rating/RatingService/RatingServiceApplication.java
+++ RatingService/RatingService/src/main/java/com/rating/RatingService/RatingServiceApplication.java
@@ -7,7 +7,5 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RatingServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RatingServiceApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(RatingServiceApplication.class, args);}
 }
